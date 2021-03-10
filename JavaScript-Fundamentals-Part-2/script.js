98df615fc35a76e9b860e36de4ed37f1d20209df
iremkenar <irem.kenar@outlook.com>
2021-03-10 15:38:42 +0300

    Added the solution for the second coding challenge

--- JavaScript-Fundamentals-Part-2/script.js
+++ JavaScript-Fundamentals-Part-2/script.js
@@ -14,10 +14,10 @@ A team ONLY wins if it has at least DOUBLE the average score of the other team. 
 
 TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
-
-
 */
-const calcAverage = (a,b,c) => (a + b + c)/3;
+
+
+/******const calcAverage = (a,b,c) => (a + b + c)/3;
 
 const checkWinner = (avgDolphins,avgKoalas) => {
     avgDolphins >= 2 * avgKoalas ? console.log(`Dolphins win 🏆 ${avgDolphins} vs ${avgKoalas}`) 
@@ -37,12 +37,11 @@ checkWinner(scoreDolphins,scoreKoalas);
 /*Could be declared as different variables as well
 const scoreDolphins2 = calcAverage(85,54,41);
 const scoreKoalas2 = calcAverage(23,34,27);
-checkWinner(scoreDolphins2,scoreKoalas2);
+checkWinner(scoreDolphins2,scoreKoalas2);******/
 
 
 
 /*****Method 2
-
 // Test 1
 const calcAverage = (a,b,c) => (a + b + c) / 3;
 
@@ -66,4 +65,32 @@ scoreKoalas = calcAverage(23, 34, 27);
 checkWinner(scoreDolphins, scoreKoalas); ******/
 
 
+///////////////////////////////////////
+// Coding Challenge #2
 
+/*
+Steven is still building his tip calculator, using the same rules as before: 
+Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, 
+calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). 
+Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44 */
+
+const calcTip = (billVal) => {
+  if (billVal >= 50 && billVal <= 300) {
+      return billVal * 0.15;
+  } return billVal * 0.2;
+}
+
+calcTip(100);
+
+const bills = [125,555,44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(tips);
+console.log(total);
