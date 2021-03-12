f96f1bf8cbbc12ee95eb071c2efbf714458a035c
iremkenar <irem.kenar@outlook.com>
2021-03-12 17:51:40 +0300

    Added highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   // convert to number because typeof input is always string
@@ -16,6 +17,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    } /*else {
+      document.querySelector('.highscore').textContent = highscore;
+    }*/
     //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
