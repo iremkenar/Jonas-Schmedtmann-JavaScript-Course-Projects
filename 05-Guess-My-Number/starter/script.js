6849d9cd7d903619b88086b511fad16ccb4e0fd2
iremkenar <irem.kenar@outlook.com>
2021-03-12 16:08:09 +0300

    Added if statements for the score

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,18 +1,34 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-  // convert to number because typeof input is string
+  // convert to number because typeof input is always string
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    document.querySelector('.message').textContent = 'Not a number!🧐';
+    //check if there is an input value and convert it to true for if to execute
+    document.querySelector('.message').textContent = 'No number!🧐';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎉🎉';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!📈';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game🤔';
+      document.querySelector('.score').textContent = score = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!📉';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game🤔';
+      document.querySelector('.score').textContent = score = 0;
+    }
   }
 });
