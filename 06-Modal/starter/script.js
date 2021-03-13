7f7b5d4d4c4111988aefb7011a607aadc5b8a894
iremkenar <irem.kenar@outlook.com>
2021-03-13 15:28:33 +0300

    Added closeModal function to close modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -10,3 +10,11 @@ for (let i = 0; i < $btnsOpenModal.length; i++)
     $modal.classList.remove('hidden');
     $overlay.classList.remove('hidden');
   });
+
+const closeModal = function () {
+  $modal.classList.add('hidden');
+  $overlay.classList.add('hidden');
+};
+
+$btnCloseModal.addEventListener('click', closeModal);
+$overlay.addEventListener('click', closeModal);
