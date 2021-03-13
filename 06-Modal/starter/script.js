e0d65f79c8be58d167b26f1aaa5365aa535c4b36
iremkenar <irem.kenar@outlook.com>
2021-03-13 15:21:38 +0300

    Added click event to buttons to open the modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const $modal = document.querySelector('.modal');
+const $overlay = document.querySelector('.overlay');
+const $btnsOpenModal = document.querySelectorAll('.show-modal');
+const $btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < $btnsOpenModal.length; i++)
+  $btnsOpenModal[i].addEventListener('click', function () {
+    $modal.classList.remove('hidden');
+    $overlay.classList.remove('hidden');
+  });
