08d76dd0fd250c657d1bfe681e6cb1dd1ec2bad1
iremkenar <irem.kenar@outlook.com>
2021-03-14 21:29:47 +0300

    Added init function to reset the game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -8,19 +8,33 @@ const $score1El = document.querySelector('#score--1');
 const $diceEl = document.querySelector('.dice');
 const $btnRoll = document.querySelector('.btn--roll');
 const $btnHold = document.querySelector('.btn--hold');
+const $btnNew = document.querySelector('.btn--new');
 const $current0El = document.querySelector('#current--0');
 const $current1El = document.querySelector('#current--1');
 
-//Initial conditions
-$score0El.textContent = 0;
-$score1El.textContent = 0;
-$diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-//State variable
-let playing = true;
+//Initial conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  //State variable
+  playing = true;
+
+  //Reset conditions
+  $score0El.textContent = 0;
+  $score1El.textContent = 0;
+  $current0El.textContent = 0;
+  $current1El.textContent = 0;
+
+  $diceEl.classList.add('hidden');
+  $player0El.classList.remove('player--winner');
+  $player1El.classList.remove('player--winner');
+  $player0El.classList.add('player--active');
+  $player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -54,6 +68,7 @@ $btnRoll.addEventListener('click', function () {
   }
 });
 
+//Keeping the score
 $btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
@@ -62,7 +77,7 @@ $btnHold.addEventListener('click', function () {
       scores[activePlayer];
     // scores[1] = scores[1] + currentScore
     // 2. Check if player's score >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //  Finish the game
       playing = false;
       $diceEl.classList.add('hidden');
@@ -78,3 +93,6 @@ $btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//Resetting the game
+$btnNew.addEventListener('click', init);
