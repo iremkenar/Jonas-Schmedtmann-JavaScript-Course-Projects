515f636e9abc8fe8ef6f323d655b68349a143d9b
iremkenar <irem.kenar@outlook.com>
2021-03-14 18:43:26 +0300

    Set active player functionality and style

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const $player0El = document.querySelector('.player--0');
+const $player1El = document.querySelector('.player--1');
 const $score0El = document.querySelector('#score--0');
 const $score1El = document.querySelector('#score--1');
 const $diceEl = document.querySelector('.dice');
@@ -13,7 +15,9 @@ $score0El.textContent = 0;
 $score1El.textContent = 0;
 $diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling the dice
 $btnRoll.addEventListener('click', function () {
@@ -29,8 +33,15 @@ $btnRoll.addEventListener('click', function () {
   if (diceNum !== 1) {
     // add diceNum to current score
     currentScore += diceNum;
-    $current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore; // of the active player
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    $player0El.classList.toggle('player--active');
+    $player1El.classList.toggle('player--active');
   }
 });
