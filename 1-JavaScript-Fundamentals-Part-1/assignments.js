3142162c7644abd376936acfbeaad6c87326b45a
iremkenar <irem.kenar@outlook.com>
2021-03-15 14:34:57 +0300

    Added solution for data types assignment

--- 1-JavaScript-Fundamentals-Part-1/assignments.js
+++ 1-JavaScript-Fundamentals-Part-1/assignments.js
@@ -1,9 +1,17 @@
 //Assignments from Jonas Schmedtmanns course
 
-//1. Values and Variables
+// 1. Values and Variables
 let country = 'Germany';
 let continent = 'Europe';
 let population = 83;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// 2. Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
