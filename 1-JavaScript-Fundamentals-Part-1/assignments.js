70fd169abc7ab3f127a11f29d96302b0986a0fa2
iremkenar <irem.kenar@outlook.com>
2021-03-15 12:06:05 +0300

    Added solution for values and variables assignment

---
+++ 1-JavaScript-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,9 @@
+//Assignments from Jonas Schmedtmanns course
+
+//1. Values and Variables
+let country = 'Germany';
+let continent = 'Europe';
+let population = 83;
+console.log(country);
+console.log(continent);
+console.log(population);
