56ab308fbb47c81621528e04f40a26eccd95e208
iremkenar <irem.kenar@outlook.com>
2021-03-20 22:18:07 +0300

    Added solutions for assignments 5, 6 and 7

--- 1-JavaScript-Fundamentals-Part-1/assignments.js
+++ 1-JavaScript-Fundamentals-Part-1/assignments.js
@@ -47,3 +47,17 @@ console.log(description);
 // 5. Strings and Template Literals
 description = `${country} is in ${continent}, and its ${gerPopulation} million people speak ${language}`;
 console.log(description);
+
+//  6. If else statements
+if (gerPopulation > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - gerPopulation} average`);
+}
+
+// 7. Type conversion and coercion
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
