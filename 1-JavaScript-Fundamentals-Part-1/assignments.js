b1d245053a3da7590404ab590b6fc0639b92aa6b
iremkenar <irem.kenar@outlook.com>
2021-03-21 11:59:35 +0300

    Added solution for the equality operators assignment

--- 1-JavaScript-Fundamentals-Part-1/assignments.js
+++ 1-JavaScript-Fundamentals-Part-1/assignments.js
@@ -61,3 +61,17 @@ console.log('19' - '13' + '17'); // 617
 console.log('19' - '13' + 17); // 23
 console.log('123' < 57); // false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+
+// 8. Equality Operators
+// Need to convert to number as the output of prompt() is string and strict equality would work
+const numNeighbours = Number(
+  prompt('Hom many neighbours does your country have?')
+);
+
+if (numNeighbours === 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');
+}
